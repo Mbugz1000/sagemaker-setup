d3d4ea5bd3fdc1b9753a350d567769fc278c7f13
Mbugz1000 <44344898+Mbugz1000@users.noreply.github.com>
2024-02-15 16:15:59 +0300

    Moving it to home directory

--- scripts/ssh/ngrok.sh
+++ scripts/ssh/ngrok.sh
@@ -1,6 +1,6 @@
 echo "Downloading ngrok..."
-curl https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz > /home/ec2-user/SageMaker/ngrok.tgz
-tar -xvzf /home/ec2-user/SageMaker/ngrok.tgz -C /home/ec2-user
+curl https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz > /home/ec2-user/ngrok.tgz
+tar -xvzf /home/ec2-user/ngrok.tgz -C /home/ec2-user
 
 echo "Creating config file /root/.config/ngrok/ngrok.yml..."
 mkdir -p /root/.config/ngrok && tee /root/.config/ngrok/ngrok.yml << EOF
