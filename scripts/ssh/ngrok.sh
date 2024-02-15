4ecf838414c69da80113a9968f7439c373421ec7
Mbugz1000 <44344898+Mbugz1000@users.noreply.github.com>
2024-02-15 15:51:51 +0300

    Changed to ngrok.yml

--- scripts/ssh/ngrok.sh
+++ scripts/ssh/ngrok.sh
@@ -2,10 +2,10 @@ echo "Downloading ngrok..."
 curl https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz > /home/ec2-user/ngrok.tgz
 tar -xvzf /home/ec2-user/ngrok.tgz -C /home/ec2-user
 
-echo "Creating config file /home/ec2-user/SageMaker/.ngrok/config.yml..."
+echo "Creating config file /home/ec2-user/SageMaker/.ngrok/ngrok.yml..."
 mkdir -p /home/ec2-user/SageMaker/.ngrok
-if [[ ! -e /home/ec2-user/SageMaker/.ngrok/config.yml ]]; then
-    cat > /home/ec2-user/SageMaker/.ngrok/config.yml << EOF
+if [[ ! -e /home/ec2-user/SageMaker/.ngrok/ngrok.yml ]]; then
+    cat > /home/ec2-user/SageMaker/.ngrok/ngrok.yml << EOF
 authtoken: $NGROK_AUTH_TOKEN
 tunnels:
     ssh:
