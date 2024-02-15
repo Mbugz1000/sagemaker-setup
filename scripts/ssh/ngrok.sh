4474a098a71970fda08c424749d7b9363ae56024
Mbugz1000 <44344898+Mbugz1000@users.noreply.github.com>
2024-02-15 15:05:07 +0300

    Update ngrok.sh x3

--- scripts/ssh/ngrok.sh
+++ scripts/ssh/ngrok.sh
@@ -1,6 +1,6 @@
 echo "Downloading ngrok..."
-curl https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-arm64.tgz > ngrok.tgz
-tar xvzf ngrok.tgz
+curl https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-arm64.tgz > /home/ec2-user/ngrok.tgz
+tar xvzf /home/ec2-user/ngrok.tgz -C /home/ec2-user/ngrok
 
 echo "Creating config file /home/ec2-user/SageMaker/.ngrok/config.yml..."
 mkdir -p /home/ec2-user/SageMaker/.ngrok
